1bd41b51f9df2a0304cc7a8b819ef1914b6628f5
Bhuvnesh verma <99537126+MasterBhuvnesh@users.noreply.github.com>
2025-02-15 19:52:40 +0530

    🐛 fix(home): fixed issue

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   reactStrictMode: true,
-  /* other config options here */
 };
 
 export default nextConfig;
